9c4cb70c1e74b732d2b6529fba5812ac2728fdc6
Jakub Boguščiak <jakub.bogusciak@student.tuke.sk>
2019-10-19 16:51:31 +0200

    Pridanie zakladneho spring boot
    pomocou web appky spring initializr.

---
+++ SensorWebApi/src/main/java/sk/tuke/SensorWebApi/SensorWebApiApplication.java
@@ -0,0 +1,13 @@
+package sk.tuke.SensorWebApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorWebApiApplication.class, args);
+	}
+
+}
---
+++ SensorWebApi/src/test/java/sk/tuke/SensorWebApi/SensorWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.tuke.SensorWebApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
