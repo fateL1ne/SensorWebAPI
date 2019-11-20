93dadf35792ca602cefd6edefc285c68fd995a62
Ján Burdeľ <jan.burdel@student.tuke.sk>
2019-11-20 22:24:37 +0100

    change encoder

--- SensorWebApi/src/main/java/sk/tuke/SensorWebApi/server/security/SecurityConfiguration.java
+++ SensorWebApi/src/main/java/sk/tuke/SensorWebApi/server/security/SecurityConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import sk.tuke.SensorWebApi.server.security.jwt.JwtRequestFilter;
